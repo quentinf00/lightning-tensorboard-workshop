c93049258d8765f97eb6ae4957e18ea901a1f3a5
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2021-09-13 17:42:30 +0200

    Add notebook

--- cifar.py
+++ cifar.py
@@ -1,3 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: region_name,title,-all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+# ---
+
+# %%
 
 # %% [md]
 """
